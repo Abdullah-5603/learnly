4247ce7b4e7442e64ef10000a3a8b11930e83b2c
Abu Abdullah <102359047+Abdullah-5603@users.noreply.github.com>
2025-01-11 14:07:06 +0600

    navigation system updated

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const currentPath = window.location.pathname;
 const navLinks = document.querySelectorAll('.learnly-navbar--routes a');
-console.log(currentPath);
+
+const currentPathDev = currentPath;
+const currentPathProduction = currentPath.split('/')[2]
 
 navLinks.forEach(link => {
-    if (link.getAttribute('href') === `.${currentPath}`) {
+    if ((link.getAttribute('href') === `.${currentPathDev}`) || (link.getAttribute('href') === `.${currentPathProduction}`)) {
         link.classList.add('active');
     } else{
         link.classList.remove('active')
